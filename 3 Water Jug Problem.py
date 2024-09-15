f72f9e82c47578f4e457a90168275b45bc98d862
charan939044 <129865777+charan939044@users.noreply.github.com>
2024-09-15 20:28:21 +0530

    Update 3 Water Jug Problem.py

--- 3 Water Jug Problem.py
+++ 3 Water Jug Problem.py
@@ -1,22 +1,35 @@
 from collections import defaultdict
-jug1, jug2, aim = 4, 3, 2
-visited = defaultdict(lambda: False)
-def waterJugSolver(amt1, amt2):
-	if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
-		print(amt1, amt2)
-		return True
-	if visited[(amt1, amt2)] == False:
-		print(amt1, amt2)
-		visited[(amt1, amt2)] = True
-		return (waterJugSolver(0, amt2) or
-				waterJugSolver(amt1, 0) or
-				waterJugSolver(jug1, amt2) or
-				waterJugSolver(amt1, jug2) or
-				waterJugSolver(amt1 + min(amt2, (jug1-amt1)),
-				amt2 - min(amt2, (jug1-amt1))) or
-				waterJugSolver(amt1 - min(amt1, (jug2-amt2)),
-				amt2 + min(amt1, (jug2-amt2))))
-	else:
-		return False
-print("Steps: ")
-waterJugSolver(0, 0)
+
+def waterJugSolver(jug1, jug2, aim, amt1, amt2, visited):
+    if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
+        print(amt1, amt2)
+        return True
+    if not visited[(amt1, amt2)]:
+        print(amt1, amt2)
+        visited[(amt1, amt2)] = True
+        return (waterJugSolver(jug1, jug2, aim, 0, amt2, visited) or
+                waterJugSolver(jug1, jug2, aim, amt1, 0, visited) or
+                waterJugSolver(jug1, jug2, aim, jug1, amt2, visited) or
+                waterJugSolver(jug1, jug2, aim, amt1, jug2, visited) or
+                waterJugSolver(jug1, jug2, aim,
+                               amt1 + min(amt2, (jug1 - amt1)),
+                               amt2 - min(amt2, (jug1 - amt1)),
+                               visited) or
+                waterJugSolver(jug1, jug2, aim,
+                               amt1 - min(amt1, (jug2 - amt2)),
+                               amt2 + min(amt1, (jug2 - amt2)),
+                               visited))
+    else:
+        return False
+
+def user_input():
+    jug1 = int(input("Enter the capacity of the first jug: "))
+    jug2 = int(input("Enter the capacity of the second jug: "))
+    aim = int(input("Enter the target amount: "))
+
+    visited = defaultdict(lambda: False)
+    print("Steps: ")
+    waterJugSolver(jug1, jug2, aim, 0, 0, visited)
+
+# Call the function to take user input and solve the problem
+user_input()
