2520d24905e36cae33e13aabb7960f779ba94295
charan939044 <129865777+charan939044@users.noreply.github.com>
2024-09-15 20:05:48 +0530

    Update 23.pl

--- 23.pl
+++ 23.pl
@@ -1,47 +1,7 @@
-parent(john, mary).
-parent(john, michael).
-parent(susan, mary).
-parent(susan, michael).
-parent(mary, alice).
-parent(mary, james).
-parent(michael, robert).
-parent(linda, robert).
-
-male(john).
-male(michael).
-male(james).
-male(robert).
-
-female(susan).
-female(mary).
-female(alice).
-female(linda).
-
-father(Father, Child) :-
-    parent(Father, Child),
-    male(Father).
-
-mother(Mother, Child) :-
-    parent(Mother, Child),
-    female(Mother).
-
-sibling(Sibling1, Sibling2) :-
-    parent(Parent, Sibling1),
-    parent(Parent, Sibling2),
-    Sibling1 \= Sibling2.
-
-grandparent(Grandparent, Grandchild) :-
-    parent(Grandparent, Parent),
-    parent(Parent, Grandchild).
-
-uncle_or_aunt(UncleOrAunt, NieceOrNephew) :-
-    sibling(UncleOrAunt, Parent),
-    parent(Parent, NieceOrNephew).
-
-uncle(Uncle, NieceOrNephew) :-
-    uncle_or_aunt(Uncle, NieceOrNephew),
-    male(Uncle).
-
-aunt(Aunt, NieceOrNephew) :-
-    uncle_or_aunt(Aunt, NieceOrNephew),
-    female(Aunt).
+parent(kanakam,charan). parent(kanakam,pavani).
+parent(charan,kalyan). parent(pavani,aditya).
+female(pavani).
+male(charan). male(kanakam). male(kalyan). male(aditya).
+father(F,C):- parent(F,C), male(F).
+mother(M,C):- parent(M,C), female(M).
+sibling(A,B):- parent(P,A), parent(P,B), A\=B.
